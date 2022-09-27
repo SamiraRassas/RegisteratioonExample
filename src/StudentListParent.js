0b3cd0e96be96b96b18a82c5197d6b3ad1ec150e
Samira Rassas <samirarassas22@gmail.com>
2022-09-27 18:16:18 +0300

    studentForm

--- src/StudentListParent.js
+++ src/StudentListParent.js
@@ -1,17 +1,24 @@
-import {useState} from "react";
+import { useState } from "react";
 import AddNewStudent from "./AddNewStudent.js";
-export function StudentListParent(){
-    const [student, setStudent]= useState('');
-    const newStudent= (childdata) => {
-        setStudent(childdata);
-    }
-    return (
-        <div>
-            <div>
-                <AddNewStudent newStudent={newStudent}/>
-            </div> 
-           <h1>'the new student is' {student}</h1>
-             </div>
-    )
+import { students } from "./students.js";
 
+export function StudentListParent() {
+  const [student, setStudent] = useState("");
+  const newStudent = (childdata) => {
+    setStudent(childdata);
+  };
+  function seeAllStudent() {
+    students.push(student);
+    JSON.stringify(students);
+  }
+
+  return (
+    <div>
+      <div>
+        <AddNewStudent newStudent={student} />
+      </div>
+      {/* <button onClick={()=>seeAllStudent()}>SEE ALL STUDENTS</button> */}
+      <div>{JSON.stringify(students)}</div>
+    </div>
+  );
 }
